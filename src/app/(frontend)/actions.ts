dea458bb524f2550a8f8970569894c7fc6b8b67a
unbaiat <unbaiat@users.noreply.github.com>
2025-10-24 08:49:17 +0300

    update

--- src/app/(frontend)/actions.ts
+++ src/app/(frontend)/actions.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export async function logout() {
-  const cookieStore = cookies()
+  const cookieStore = await cookies()
 
   for (const cookie of cookieStore.getAll()) {
     if (cookie.name.startsWith('payload-')) {
